529738a8cf5b95a86be8981fc0b1327629f52a9a
Fernando Crespo Sanchez <fernando@rti.com>
2024-10-11 09:00:45 -0700

    Add the ability to start external apps

--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,7 @@ import io from "socket.io-client";
 import { Socket } from "socket.io-client";
 import { v4 as uuidv4 } from 'uuid';
 import * as fs from 'fs';
+import { exec } from 'child_process';
 
 class GlobalState {
     readonly connextProduct: string;
@@ -384,6 +385,81 @@ async function generateFollowUps(num_followups: number, conversation: string, to
 }
 
 /**
+ */
+async function generateBotFollowUp(conversation: string, token: vscode.CancellationToken): Promise<string | null> {
+    const QUESTION = `
+    You are an RTI Connext chatbot tasked with generating a potential follow-up
+    question from you to the RTI Connext developer based on the context of an
+    ongoing conversation and a developer question/request. The developer may be 
+    new to RTI Connext or have limited experience with the technology. They are 
+    seeking guidance on how to advance their project or resolve specific issues. 
+    Your goal is to generate a concise and relevant follow-up question that you 
+    could ask the developer, based on the full context of the conversation. This 
+    question should help keep the developer engaged and guide them towards 
+    useful solutions. Only generate one follow-up question if you feel
+    you need more information to answer the developer's question/request.
+
+    Only generate follow-up questions for this topics:
+    - IDL and XML type design.
+    - rtiddsgen and code generation for parameters that are unknown and need to 
+    be defined by the user.
+
+    For example:
+
+    User question: Can you provide an example IDL?
+    Bot question: What information are you looking for in the IDL?
+
+    For example:
+
+    User question: How to compile an IDL file?
+    Bot question: What language are you using to compile the IDL file?
+
+    Output the question in valid JSON format, structured as follows:
+
+    If there is a follow-up question:
+    {
+        "questions": [
+            {
+            "question": "Developer's potential follow-up question here",
+            "summary": "Concise 7-word summary of the question"
+            }
+        ]
+    }
+
+    If there is no follow-up question:
+    {
+        "questions": []
+    }
+
+    conversation START:
+    ${conversation}
+    conversation END
+    `;
+
+    let response = await askQuestion(QUESTION, token);
+
+    response = response.replace('```json', '');
+    response = response.replace('```', '');
+
+    let parsedData;
+
+    try {
+        parsedData = JSON.parse(response);
+    } catch (error) {
+        /* Any error leads to an empty followup */
+        return null;
+    }
+
+    let followup = null;
+
+    if (parsedData.questions.length > 0) {
+        followup = parsedData.questions[0].question;
+    }
+
+    return followup;
+}
+
+/**
  * Analyzes the provided response to determine the types of source code content present.
  * 
  * @param response - The response string to analyze for source code content.
@@ -444,6 +520,53 @@ async function getCodeContentInfo(response: string, token: vscode.CancellationTo
     }
 
     return languagesInResponse;
+}
+
+/**
+ * Determines if a given prompt is related to the application_name.
+ *
+ * @param application_name - The name of the application to be evaluated.
+ * @param prompt - The prompt string to be evaluated.
+ * @param token - A VS Code cancellation token to handle cancellation.
+ * @returns A promise that resolves to a boolean indicating whether the prompt is related to the application_name.
+ */
+async function isPromptRelatedToApplication(application_name: string, prompt: string, token: vscode.CancellationToken): Promise<boolean> {
+    const QUESTION = `
+    Determine if the following prompt refers to the ${application_name} application:
+    <<BEGIN>>
+    ${prompt}
+    <<END>>
+
+    The output should be in JSON format as follows:
+    {
+        "result": true|false
+    }
+    `;
+
+    let applicationResponse = await askQuestion(QUESTION, token);
+
+    applicationResponse = applicationResponse.replace('```json', '');
+    applicationResponse = applicationResponse.replace('```', '');
+
+    let parsedData;
+
+    try {
+        parsedData = JSON.parse(applicationResponse);
+        return parsedData.result;
+    } catch (error) {
+        return false;
+    }
+}
+
+function runApplication(applicationName: string) {
+    // Use child_process.exec to run the external application
+    exec(applicationName, (err, stdout, stderr) => {
+        if (err) {
+            // Handle the error
+            vscode.window.showErrorMessage(`Error running command: ${err.message}`);
+            return;
+        }
+    });
 }
 
 /**
@@ -541,7 +664,35 @@ export function activate(context: vscode.ExtensionContext) {
         const VALIDATE_CODE_PROMPT = globalState.VALIDATE_CODE_PROMPT_PREFIX + " " + languages + " code and provide the updated code"
         vscode.commands.executeCommand('workbench.action.chat.open', `@connext ${VALIDATE_CODE_PROMPT}`);
     });
+
+    // Run Admin Console
+    vscode.commands.registerCommand('connext-vc-copilot.run-admin-console', () => {
+        const command = 'rtiadminconsole';
+
+        runApplication(command);
+    });
     
+    // Run System Designer
+    vscode.commands.registerCommand('connext-vc-copilot.run-system-designer', () => {
+        const command = 'rtisystemdesigner';
+
+        runApplication(command);
+    });
+
+    // Run Monitor
+    vscode.commands.registerCommand('connext-vc-copilot.run-monitor-ui', () => {
+        const command = 'rtimonitor';
+
+        runApplication(command);
+    });
+
+    // Run Shape Demo
+    vscode.commands.registerCommand('connext-vc-copilot.run-shapes-demo', () => {
+        const command = 'rtishapesdemo';
+
+        runApplication(command);
+    });
+
     globalThis.globalState.storedUsername = context.globalState.get<string>(globalThis.globalState.connextUsernameKey);
     globalThis.globalState.storedPassword = context.globalState.get<string>(globalThis.globalState.connextPasswordKey);
 
@@ -591,6 +742,31 @@ export function activate(context: vscode.ExtensionContext) {
 
             globalThis.globalState.accessCode = jsonResponse.access_token;
         }
+
+        if (request.command === 'start-admin-console') {
+            runApplication('rtiadminconsole');
+            response.markdown("Starting RTI Admin Console...");
+            return
+        } else if (request.command === 'start-system-designer') {
+            runApplication('rtisystemdesigner');
+            response.markdown("Starting RTI System Designer...");
+            return
+        } else if (request.command === 'start-monitor-ui') {
+            runApplication('rtimonitor');
+            response.markdown("Starting RTI Monitor...");
+            return
+        } else if (request.command === 'start-shapes-demo') {
+            runApplication('rtishapesdemo');
+            response.markdown("Starting RTI Shapes Demo...");
+            return 
+        }
+
+        //let botFollowup = await generateBotFollowUp(getPrompt(globalState.lastPrompt, null, context), token);
+        
+        //if (botFollowup != null) {
+        //    response.markdown(botFollowup);
+        //    return;
+        //}
 
         let socket;
         globalState.lastResponse = "";
@@ -687,6 +863,40 @@ export function activate(context: vscode.ExtensionContext) {
                 });
             }
 
+            if (await isPromptRelatedToApplication("RTI Admin Console", globalState.lastPrompt, token)) {
+                response.markdown(`\n\n*Click 'Run Admin Console' to open the RTI Admin Console from Visual Code.*`);
+                
+                response.button({
+                    command: 'connext-vc-copilot.run-admin-console',
+                    title: vscode.l10n.t('Run Admin Console'),
+                    arguments: []
+                });
+            } else if (await isPromptRelatedToApplication("RTI System Designer", globalState.lastPrompt, token)) {
+                response.markdown(`\n\n*Click 'Run System Designer' to open the RTI System Designer from Visual Code.*`);
+
+                response.button({
+                    command: 'connext-vc-copilot.run-system-designer',
+                    title: vscode.l10n.t('Run System Designer'),
+                    arguments: []
+                });
+            } else if (await isPromptRelatedToApplication("RTI Monitor", globalState.lastPrompt, token)) {
+                response.markdown(`\n\n*Click 'Run Monitor' to open the RTI Monitor from Visual Code.*`);
+
+                response.button({
+                    command: 'connext-vc-copilot.run-monitor-ui',
+                    title: vscode.l10n.t('Run Monitor'),
+                    arguments: []
+                });
+            } else if (await isPromptRelatedToApplication("RTI Shapes Demo", globalState.lastPrompt, token)) {
+                response.markdown(`\n\n*Click 'Run Shapes Demo' to open the RTI Shapes Demo from Visual Code.*`);
+
+                response.button({
+                    command: 'connext-vc-copilot.run-shapes-demo',
+                    title: vscode.l10n.t('Run Shapes Demo'),
+                    arguments: []
+                });
+            }
+
             if (validationPrompt) {
                 response.markdown(`\n\n***NOTE:** Although the code has been validated, it may still contain errors. Please review and test the code before using it.*`);
             }
@@ -698,7 +908,9 @@ export function activate(context: vscode.ExtensionContext) {
     chat.followupProvider = {
         async provideFollowups(
             result: IChatResult, context: vscode.ChatContext, token: vscode.CancellationToken) {
-            return await generateFollowUps(globalState.NUM_FOLLOWUPS, getPrompt(globalState.lastPrompt, globalState.lastResponse, context), token);
+            if (result.metadata.command != 'start-admin-console') {
+                return await generateFollowUps(globalState.NUM_FOLLOWUPS, getPrompt(globalState.lastPrompt, globalState.lastResponse, context), token);
+            }
         }
     };
 
